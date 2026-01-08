fc85e331b74467c02694a03cf7b7cca574e73945
MESA <66805616+MESAresearch@users.noreply.github.com>
2026-01-08 01:06:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use('/public', express.static('public'));
-app.set('trust proxy', true); //this is to make the real ips come through
-
 
 app.get("/",function(req,res){
-	res.sendFile(__dirname+"/public/index.html");
+	res.send("hello world!");
 });
 
 
